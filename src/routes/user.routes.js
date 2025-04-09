c1d5efaff1f53e399cdb1689396c58db311e63bc
Raunak Kumar <kumarraunak085@gmail.com>
2025-04-10 03:37:19 +0530

    implement user authentication: add login, logout, and token refresh functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,15 @@
 import {Router} from 'express';
-import { userRegister } from '../controllers/register.controller.js';
+import { logoutUser, refreshAccessToken, userRegister } from '../controllers/register.controller.js';
 import { upload } from '../middleware/mutler.js';
+import { loginUser } from '../controllers/register.controller.js';
+import { verifyJWT } from '../middleware/auth.middleware.js';
 
 const userRouter= Router();
 
 userRouter.route("/register").post(
     upload.fields([ // this is a middleware which is used to upload files
         // upload.single("avatar"), // this is used to upload single file
-        
+
         {
             name: "avatar",
             maxCount: 1
@@ -19,6 +21,10 @@ userRouter.route("/register").post(
     ]),
     userRegister
     );
+
+    userRouter.route("/login").post(loginUser);
+    userRouter.route("/logout").post(verifyJWT,logoutUser);
+    userRouter.route("/refresh-token").post(refreshAccessToken);
 
 export {userRouter}
 
